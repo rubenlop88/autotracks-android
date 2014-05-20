fc1fb2ddc3627c771b9d7c164207f4eb30e1d25c
Alfredo Campuzano <bandit.adcr@gmail.com>
2014-05-19 21:32:47 -0400

    Se agregó la posibilidad de ver las rutas con un webview

--- autotracks/src/main/java/py/com/fpuna/autotracks/RutaDetailFragment.java
+++ autotracks/src/main/java/py/com/fpuna/autotracks/RutaDetailFragment.java
@@ -4,10 +4,16 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,6 +22,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +39,7 @@ import retrofit.RestAdapter;
 
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
-public class RutaDetailFragment extends SupportMapFragment {
+public class RutaDetailFragment extends Fragment {
 
     public static final String EXTRA_RUTA_ID = "ruta_id";
 
@@ -42,6 +52,7 @@ public class RutaDetailFragment extends SupportMapFragment {
     }
 
     private String rutaId;
+    private WebView webView = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -129,18 +140,35 @@ public class RutaDetailFragment extends SupportMapFragment {
     }
 
     private void initMap() {
-        Iterable<Localizacion> localizacions = getLocalizacionesIterable();
-        if (localizacions.iterator().hasNext()) {
-            PolylineOptions polyline = new PolylineOptions();
-            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            for (Localizacion localizacion : localizacions) {
-                LatLng position = new LatLng(localizacion.getLatitud(), localizacion.getLongitud());
-                getMap().addMarker(new MarkerOptions().position(position));
-                builder.include(position);
-                polyline.add(position);
-            }
-            getMap().addPolyline(polyline.width(5));
-            moveCamera(builder);
+        if (webView == null) {
+            webView = (WebView) getView().findViewById(R.id.webViewRuta);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebViewClient(new WebViewClient() {
+
+                public void onPageFinished(WebView view, String url) {
+                    Iterable<Localizacion> localizacions = getLocalizacionesIterable();
+
+                    JSONArray jsonArray = new JSONArray();
+                    JSONObject jsonObject = new JSONObject();
+
+                    try {
+                        if (localizacions.iterator().hasNext()) {
+                            for (Localizacion localizacion : localizacions) {
+                                jsonObject = new JSONObject();
+                                jsonObject.put("latitud", localizacion.getLatitud());
+                                jsonObject.put("longitud", localizacion.getLongitud());
+                                jsonArray.put(jsonObject);
+                            }
+                        }
+                        jsonObject = new JSONObject();
+                        jsonObject.put("ruta", jsonArray);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    view.loadUrl("javascript:dibujarRuta(" + jsonObject.toString() + ");");
+                }
+            });
+            webView.loadUrl("file:///android_asset/rutas.html");
         }
     }
 
@@ -153,7 +181,7 @@ public class RutaDetailFragment extends SupportMapFragment {
                 } else {
                     getView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 }
-                getMap().moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 50));
+                //getMap().moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 50));
             }
         });
     }
@@ -165,4 +193,9 @@ public class RutaDetailFragment extends SupportMapFragment {
                 .query();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mainView = (View) inflater.inflate(R.layout.activity_ruta_detail, container, false);
+        return mainView;
+    }
 }
